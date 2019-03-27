acff63310c800178a107847db5d6e70bf85ed334
leetaylor7 <ltaylor@goodmanfinancial.com>
2019-03-26 21:21:52 -0500

    got the charts to work

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -1,1 +1,126 @@
 // @TODO: YOUR CODE HERE!
+// Define SVG area dimensions
+var svgWidth = 820;
+var svgHeight = 460;
+
+var margin = {
+    top: 20,
+    right: 40,
+    bottom: 60,
+    left: 100
+  };
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+
+// Select body, append SVG area to it, and set the dimensions
+var svg = d3
+  .select("#scatter")
+  .append("svg")
+  .attr("height", svgHeight)
+  .attr("width", svgWidth);
+
+// Append a group to the SVG area and shift ('translate') it to the right and down to adhere
+// to the margins set in the "chartMargin" object.
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Load data from data.csv
+d3.csv("assets/data/data.csv")
+    .then(function(econData) {
+  
+// Step 1: Parse Data/Cast as numbers
+    // ==============================
+    econData.forEach(function(data) {
+        data.healthcare = +data.healthcare;
+        data.poverty = +data.poverty;
+        data.abbr = String(data.abbr);
+
+        console.log(data);
+      });
+  
+      // Step 2: Create scale functions
+      // ==============================
+      var xLinearScale = d3.scaleLinear()
+        .domain([d3.min(econData, d => d.poverty) - 1 , d3.max(econData, d => d.poverty) + 1])
+        .range([0, width]);
+  
+      var yLinearScale = d3.scaleLinear()
+        .domain([d3.min(econData, d => d.healthcare) - 1 , d3.max(econData, d => d.healthcare) + 1])
+        .range([height, 0]);
+  
+      // Step 3: Create axis functions
+      // ==============================
+      var bottomAxis = d3.axisBottom(xLinearScale);
+      var leftAxis = d3.axisLeft(yLinearScale);
+  
+      // Step 4: Append Axes to the chart
+      // ==============================
+      chartGroup.append("g")
+        .attr("transform", `translate(0, ${height})`)
+        .call(bottomAxis);
+  
+      chartGroup.append("g")
+        .call(leftAxis);
+  
+      // Step 5: Create Circles
+      // ==============================
+      var theCircles = svg.selectAll("g chartGroup").data(econData).enter();
+
+      var circlesGroup = theCircles
+        .append("circle")
+        .attr("cx", d => xLinearScale(d.poverty) + margin.left)
+        .attr("cy", d => yLinearScale(d.healthcare))
+        .attr("r", "15")
+        .attr("fill", "pink")
+        .attr("opacity", ".5");
+    
+      theCircles
+        .append("text")
+        .attr("x", d => xLinearScale(d.poverty) + margin.left- 7.5)
+        .attr("y", d => yLinearScale(d.healthcare))
+        .text(d => d.abbr)
+        .attr("font-family", "sans-serif")
+        .attr("font-size", "10px")
+        .attr("fill", "gray");
+  
+      // Step 6: Initialize tool tip
+      // ==============================
+      var toolTip = d3.tip()
+        .attr("class", "d3-tip")
+        .offset([80, -60])
+        .html(function(d) {
+          return (`Poverty Rate: ${d.poverty}<br>Healthcare Quality${d.healthcare}`);
+        });
+  
+      // Step 7: Create tooltip in the chart
+      // ==============================
+      chartGroup.call(toolTip);
+  
+      // Step 8: Create event listeners to display and hide the tooltip
+      // ==============================
+      circlesGroup.on("click", function(data) {
+        toolTip.show(data, this);
+      })
+        // onmouseout event
+        .on("mouseout", function(data, index) {
+          toolTip.hide(data);
+        });
+    
+      // Create axes labels
+      chartGroup.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left + 40)
+        .attr("x", 0 - (height / 2))
+        .attr("dy", "1em")
+        .attr("class", "axisText")
+        .text("Healthcare Quality");
+  
+      chartGroup.append("text")
+        .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+        .attr("class", "axisText")
+        .text("Poverty Rate");;
+        
+
+    });
